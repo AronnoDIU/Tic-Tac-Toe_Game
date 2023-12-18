8fc16f29f92c5ea27ec2178e809cbc74299fe374
Yeasir Arafat Aronno <yeasir35-1501@diu.edu.bd>
2023-12-19 01:20:00 +0600

    Tic-Tac-Toe Game

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -30,48 +30,69 @@ public class TicTacToe {
             if (currentPlayer == 'X') {
                 playerTurn(userInput);
             } else {
-                aiTurn(); // if it's the computerizing turn (O), make a random move
+                AI_Turn(); // if it's the computerizing turn (O), make a random move
             }
             printBoard(); // print the board after the user's move
             checkGameEnd(); // check if the game has ended
+
+            // Switch players (X to O, O to X) after each move if the game hasn't ended
             currentPlayer = (currentPlayer == 'X') ? 'O' : 'X';
         }
     }
 
     // Prompt the user for a row and column to place their move
     private void playerTurn(Scanner scanner) {
-        int row, col;
+        int row, column; // row and column to place the user's move
         do {
             System.out.println("Player " + currentPlayer + ", enter row and column (1-3):");
+
+            // Subtract 1 from the user's input since the board is 0-indexed
             row = scanner.nextInt() - 1;
-            col = scanner.nextInt() - 1;
-        } while (row < 0 || col < 0 || row >= 3 || col >= 3 || board[row][col] != ' ');
 
-        board[row][col] = currentPlayer;
+            // Subtract 1 from the user's input since the board is 0-indexed
+            column = scanner.nextInt() - 1;
+
+            // Keep prompting the user for a valid move until they enter a valid move
+        } while (row < 0 || column < 0 || row >= 3 || column >= 3 || board[row][column] != ' ');
+
+        board[row][column] = currentPlayer; // place the user's move on the board
     }
 
-    private void aiTurn() {
-        int row, col;
+    private void AI_Turn() {
+        int row, column; // row and column to place the AI's move
         do {
-            row = random.nextInt(3);
-            col = random.nextInt(3);
-        } while (board[row][col] != ' ');
+            row = random.nextInt(3); // generate random row
+            column = random.nextInt(3); // generate random column
 
-        System.out.println("AI placed " + currentPlayer + " in position " + (row + 1) + ", " + (col + 1));
-        board[row][col] = currentPlayer;
+            // Keep generating random moves until the AI finds a valid move
+        } while (board[row][column] != ' ');
+
+        // Place the AI's move on the board and print it out for the user
+        System.out.println("AI placed " + currentPlayer + " in position " + (row + 1) + ", " + (column + 1));
+        board[row][column] = currentPlayer; // place the AI's move on the board
     }
 
+    // Print the board to the console
     private void printBoard() {
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
-                System.out.print(board[i][j]);
-                if (j < 2) System.out.print("|");
-            }
-            System.out.println();
-            if (i < 2) System.out.println("-+-+-");
-        }
+        System.out.println("-------------");
+        System.out.println("| " + board[0][0] + " | " + board[0][1] + " | " + board[0][2] + " |");
+        System.out.println("-------------");
+        System.out.println("| " + board[1][0] + " | " + board[1][1] + " | " + board[1][2] + " |");
+        System.out.println("-------------");
+        System.out.println("| " + board[2][0] + " | " + board[2][1] + " | " + board[2][2] + " |");
+        System.out.println("-------------");
     }
+//        for (int i = 0; i < 3; i++) {  // print the board Optimal
+//            for (int j = 0; j < 3; j++) {
+//                System.out.print(board[i][j]);
+//                if (j < 2) System.out.print("|");
+//            }
+//            System.out.println();
+//            if (i < 2) System.out.println("-+-+-");
+//        }
+//    }
 
+    // Check if the game has ended and print the winner (if any)
     private void checkGameEnd() {
         // Check rows, columns and diagonals
         for (int i = 0; i < 3; i++) {
@@ -93,7 +114,7 @@ public class TicTacToe {
             return;
         }
 
-        // Check for tie
+        // Check for tie - if there are no empty spaces left on the board
         boolean boardFull = true;
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
